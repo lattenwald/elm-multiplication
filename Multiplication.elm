5c431a3ecd4473f46a3616c36766e41e14e652c4
Alexander Kyusev <a.kyusev@corp.mail.ru>
2016-02-18 15:37:45 +0300

    Module documentation

--- Multiplication.elm
+++ Multiplication.elm
@@ -1,36 +1,68 @@
 module Multiplication where
 
+{-| Multiplication
+
+# Types
+@docs Problem
+
+# Table format
+@docs cols, rows
+
+# Example bounds
+@docs minA, minB, maxA, maxB
+
+# RNG seed
+@docs intSeed
+
+# functions
+@docs problems, showParams, showProblem, split, main
+
+-}
+
 import Html exposing (..)
 import Html.Attributes exposing (id, class, property)
 import Random
 import Json.Encode as Json
+import Signal exposing (..)
 
 -- types
+{-| @docs problem type -}
 type alias Problem = (Int, Int)
 
 -- settings
+{-| @docs number of columns in examples table -}
 cols : Int
 cols = 2
 
+{-| @docs number of rows in examples table -}
 rows : Int
 rows = 25
 
+{-| @docs minimum A -}
 minA : Int
 minA = 2
 
+{-| @docs mimunum B -}
 minB : Int
 minB = 2
 
+{-| @docs maximum A -}
 maxA : Int
 maxA = 10
 
+{-| @docs maximum B -}
 maxB : Int
 maxB = 10
 
+{-| @docs RNG seed -}
 intSeed : Int
 intSeed = 1234
 
+-- port initialLocation : String -- incoming
+-- port location : Signal String -- outgoing
+
 -- functions
+{-| @docs problems generator -}
 problems : Int -> Random.Generator (List Problem)
 problems quantity =
   let genA = Random.int minA maxA
@@ -38,6 +70,7 @@ problems quantity =
       genPair = Random.pair genA genB
   in Random.list quantity genPair
 
+{-| @docs render problem -}
 showProblem : Problem -> Html
 showProblem (a, b) =
   div [class "problem"]
@@ -46,15 +79,16 @@ showProblem (a, b) =
       , text (toString b)
       , text " = "]
 
--- split : Int -> List a -> List (List a)
+{-| @docs split list -}
+split : Int -> List a -> List (List a)
 split n ls =
   let
-    rest : List a
     rest = List.drop n ls
   in if List.isEmpty rest
      then [ls]
      else List.take n ls :: split n rest
 
+{-| @docs render params -}
 showParams : Html
 showParams = p []
   [ text "seed = ", text <| toString intSeed
@@ -62,6 +96,7 @@ showParams = p []
   , text ", b = ", text <| toString minB, text " to ", text <| toString maxB
   , text ", table of ", text <| toString cols, text "x", text <| toString rows ]
 
+{-| @docs main render function -}
 main : Html
 main =
   let seed = Random.initialSeed intSeed
@@ -72,9 +107,3 @@ main =
                List.map (\row -> tr [] <|
                            List.map (\p -> td [] [showProblem p]) row) prob'
        , showParams ]
-
--- port initialLocation : String -- incoming
--- port location : Signal String -- outgoing
-
--- textContent : Channel Content
--- textContent = channel {noContent | string <- initialLocation}
